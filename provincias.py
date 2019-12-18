80e9b6baf2020b096a4de474f4b681b7cf7b4697
dblancoformacion <dblancoformacion@gmail.com>
2019-12-18 12:37:25 +0100

    Diccionario en vez de tupla

--- provincias.py
+++ provincias.py
@@ -1,7 +1,7 @@
 import math
 import pymysql
 con = pymysql.connect('localhost','root','','provincias')
-conn = con.cursor()
+conn = con.cursor(pymysql.cursors.DictCursor)
 conn.execute("SELECT * FROM provincias;")
 provincias=conn.fetchall()
 # Resultado de la consulta
@@ -10,32 +10,38 @@ if 0:
 # (01) Listado de provincias
 if 0:
 	for p in provincias:
-		print(p[1])
+		print(p['provincia'])
 # (02) ¿Cuánto suman 2 y 3?
 if 0:
 	print(2+3)
 # (04) Densidades de población de las provincias
 if 0:
 	for p in provincias:
-		print('{} {}'.format(p[1], round(p[2]/p[3],4) ) )
+		print('{} {}'.format(p['provincia'], round(p[2]/p[3],4) ) )
 # (52) Obtén el listado del nombre de las provincias que tiene cada autonomía
 if 0:
 	for p in provincias:
-		print('{} {}'.format(p[0], p[1] ) )
+		print('{} {}'.format(p['autonomia'], p['provincia'] ) )
 # (03) ¿Cuánto vale la raíz cuadrada de 2?
 if 0:
 	print(round(math.sqrt(2),6))
 # (05) ¿Cuántos caracteres tiene cada nombre de provincia?
 if 0:
 	for p in provincias:
-		print('{} {}'.format(p[1],len(p[1])))
+		print('{} {}'.format(p['provincia'],len(p['provincia'])))
 # (07) Provincias con el mismo nombre que su comunidad autónoma
 if 0:
 	for p in provincias:
-		if p[0]==p[1]:
-			print(p[1])
+		if p['autonomia']==p['provincia']:
+			print(p['provincia'])
 # (08) Provincias que contienen el diptongo 'ue'
+if 0:
+	for p in provincias:
+		if p['provincia'].find('ue')>0:
+			print(p['provincia'])
+# (09) Provincias que empiezan por A
 if 1:
 	for p in provincias:
-		if p[1].find('ue')>0:
-			print(p[1])
+		if p['provincia'][0]=='A' or p['provincia'][0]=='Á':
+			print(p['provincia'])
+# (17) ¿Qué provincias están en autonomías con nombre compuesto?
