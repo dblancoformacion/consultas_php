083fc04115030889ea3a6d5aa21c5ab0f5d90623
dblancoformacion <dblancoformacion@gmail.com>
2019-12-18 12:49:29 +0100

    Más consulta en python

--- provincias.py
+++ provincias.py
@@ -40,8 +40,72 @@ if 0:
 		if p['provincia'].find('ue')>0:
 			print(p['provincia'])
 # (09) Provincias que empiezan por A
-if 1:
+if 0:
 	for p in provincias:
 		if p['provincia'][0]=='A' or p['provincia'][0]=='Á':
 			print(p['provincia'])
 # (17) ¿Qué provincias están en autonomías con nombre compuesto?
+if 0:
+	for p in provincias:
+		if p['autonomia'].find(' ')>0:
+			print(p['provincia'])
+# '18'=>"¿Qué provincias tienen nombre compuesto?",
+if 0:
+	for p in provincias:
+		if p['provincia'].find(' ')>0:
+			print(p['provincia'])
+# '19'=>"¿Qué provincias tienen nombre simple?",
+if 0:
+	for p in provincias:
+		if p['provincia'].find(' ')<0:
+			print(p['provincia'])
+# '51'=>"Muestra las provincias de Galicia, indicando si es Grande, Mediana o Pequeña en función ",
+if 1:
+	for p in provincias:
+		if p['autonomia']=='Galicia':
+			if p['poblacion']>1e6:
+				r='Grande'
+			else:
+				if p['poblacion']>5e5:
+					r='Mediana'
+				else:
+					r='Pequeña'
+			print('{} {}'.format(p['provincia'],r))
+# '10'=>"Autonomías terminadas en 'ana'",
+# '11'=>"¿Cuántos caracteres tiene cada nombre de comunidad autónoma? Ordena el resultado por el ",
+# '12'=>"¿Qué autonomías tienen nombre compuesto? Ordena el resultado alfabéticamente en orden ",
+# '13'=>"¿Qué autonomías tienen nombre simple? Ordena el resultado alfabéticamente en orden ",
+# '14'=>"¿Qué autonomías tienen provincias con nombre compuesto? Ordenar el resultado ",
+# '15'=>"Autonomías que comiencen por 'can' ordenadas alfabéticamente",
+# '16'=>"¿Qué autonomías tienen provincias de más de un millón de habitantes? Ordénalas ",
+# '21'=>"Población del país",
+# '22'=>"Superficie del país",
+# '23'=>"¿Cuántas provincias hay en la tabla?",
+# '24'=>"En un listado alfabético, ¿qué provincia estaría la primera?",
+# '25'=>"¿Qué comunidades autónomas contienen el nombre de una de sus provincias?",
+# '26'=>"¿Qué provincias tienen un nombre más largo que el de su autonomía?",
+# '27'=>"¿Cuántas comunidades autónomas hay?",
+# '29'=>"¿Cuánto mide el nombre de autonomía más corto?",
+# '30'=>"¿Cuánto mide el nombre de provincia más largo?",
+# '28'=>"Población media de las provincias entre 2 y 3 millones de habitantes sin decimales",
+# '53'=>"Obtén el listado de autonomías: una línea por autonomía, junto al listado de sus provincias",
+# '31'=>"Provincia más poblada",
+# '32'=>"Provincia más poblada de las inferiores a 1 millón de habitantes",
+# '34'=>"Provincia menos poblada de las superiores al millón de habitantes",
+# '35'=>"¿En qué autonomía está la provincia más extensa?",
+# '36'=>"¿Qué provincias tienen una población por encima de la media nacional?",
+# '37'=>"Densidad de población del país",
+# '38'=>"¿Cuántas provincias tiene cada comunidad autónoma?",
+# '39'=>"Listado del número de provincias por autonomía ordenadas de más a menos provincias y ",
+# '40'=>"¿Cuántas provincias con nombre compuesto tiene cada comunidad autónoma?",
+# '41'=>"Autonomías uniprovinciales",
+# '42'=>"¿Qué autonomía tiene 5 provincias?",
+# '43'=>"Población de la autonomía más poblada",
+# '44'=>"¿Qué porcentaje del total nacional representa Cantabria en población y en superficie?",
+# '49'=>"Obtener la provincia más poblada de cada comunidad autónoma, indicando la población de ",
+# '47'=>"¿En qué posición del ranking autonómico por población de mayor a menor está Cantabria?",
+# '45'=>"Automía más extensa",
+# '46'=>"Autonomía con más provincias",
+# '48'=>"Provincia más despoblada de la autonomía más poblada",
+# '50'=>"Provincia más poblada de la autonomía más despoblada",
+# '06'=>"Listado de autonomías",
