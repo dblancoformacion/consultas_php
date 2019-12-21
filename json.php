f5178d59c82ffef7f8d402d8afc4f5958ba0f93e
dblancoformacion <dblancoformacion@gmail.com>
2019-12-21 20:25:01 +0100

    Control de visitas con async/await

--- json.php
+++ json.php
@@ -1,9 +1,18 @@
 <?php
 //header('Content-Type: application/json');
 header('Content-type:application/json;charset=utf-8');
-if(1){	// conexión a la base de datos y generación del array $provincias
+if(1){	// conexión a la base de datos
 	$conn = new mysqli('localhost','root','','provincias');
 	$conn->query("SET NAMES utf8;");
+}
+if(isset($_GET['id'])){	// incremento de visitas
+	$conn->query("
+		UPDATE provincias set visitas=visitas+1
+			WHERE id=".($_GET['id']*1).";
+	");
+	echo json_encode([1]);
+}
+else{	// generación del array $provincias
 	$provincias=$conn->query("
 		SELECT * FROM provincias;
 	")->fetch_all(MYSQLI_ASSOC);
